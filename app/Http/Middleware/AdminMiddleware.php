dc8c37c99cc0bcded7d0ee751f5643d516977c7b
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2026-04-27 14:20:10 +0330

    create middleware for check permission access

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = \auth()?->user();
+
+        if(isset($user) and $user->is_admin){
+            return $next($request);
+        }
+
+        return Redirect::route('authP', ['signL' => 'you dont have permission access to this page']);
+    }
+}
