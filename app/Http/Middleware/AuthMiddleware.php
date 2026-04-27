173a26e46a6eeca8abd7840541571c85947b13ed
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2026-04-27 14:26:24 +0330

    create middleware auth for check login

---
+++ app/Http/Middleware/AuthMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = auth()?->user();
+
+        if(isset($user))
+            return $next($request);
+
+        return redirect()->route('authP', ['signL' => 'Please login first']);
+    }
+}
