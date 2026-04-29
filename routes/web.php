adf0a8a7b6bf9b445d01dae1b24d0fe4499e3bbb
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2026-04-29 21:09:21 +0330

    add route for user

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,17 @@ Route::group([], function (){
 //Auth
 Route::middleware('Auth')->group(function () {
 
+    //chatroom
+    Route::get('/index', []);
+
     //Admin access
     Route::middleware('Admin')->group(function () {
         Route::get('/dashboard/user', [\App\Models\User::class, 'index'])->name('user.index');
+
+        //user change access and role
+        Route::put('/user/{user}/access', [\App\Http\Controllers\UserController::class, 'access_change'])->name('users.toggleAccess');
+        Route::put('/user/{user}/role', [\App\Http\Controllers\UserController::class, 'role_change'])->name('users.changeRole');
+        Route::delete('/user/{user}', [\App\Http\Controllers\UserController::class, 'destroy'])->name('users.destroy');
     });
 });
 
