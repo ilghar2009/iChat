72babad6f58f3a5b81303424ec4c6997154d69d9
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2026-05-02 13:09:30 +0330

    create SSE controller

--- app/Http/Controllers/SseController.php
+++ app/Http/Controllers/SseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class SseController extends Controller
@@ -14,6 +15,29 @@ class SseController extends Controller
         while(true){
             if(connection_aborted()){
                 break;
+            }
+
+            $newMessage = Message::where('id', '>', $lastId)
+                ->orderBy('id', 'asc')
+                ->get();
+
+            if(isset($newMessage)){
+                foreach($newMessage as $message){
+                    echo "id: {$message->id}\n";
+                    echo "data: " . json_encode([
+                            'id' => $message->id,
+                            'user_id' => $message->user_id,
+                            'user_name' => $message->user->name ?? 'Unknown', // نام کاربر
+                            'body' => $message->body,
+                            'created_at' => $message->created_at->diffForHumans() // زمان نسبی مثل "۲ دقیقه پیش"
+                        ]) . "\n\n";
+
+                    // ارسال داده‌ها به کلاینت
+                    flush();
+
+                    // آپدیت کردن lastId برای اینکه دفعه بعد فقط پیام‌های جدیدتر را بگیرد
+                    $lastId = $message->id;
+                }
             }
         }
     }
